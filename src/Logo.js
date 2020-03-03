158bb88b7033fc3c75aabd257065428336728c66
Maxence Dalmais <github@maxired.fr>
2020-03-03 08:02:07 +0100

    initial commit

---
+++ src/Logo.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import uclyLogo from './assets/Logo_ucly_couleur_RVB.png'
+
+const Logo = () => <div style={{textAlign: 'center'}}><img src={uclyLogo} height={279/2} width={394/2} /></div>
+
+export default Logo
---
+++ src/Preview.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import preview from './assets/preview.png'
+
+const Preview = () => <div style={{textAlign: 'center'}}><img src={preview} height={605/2} width={1351/2} /></div>
+
+export default Preview
